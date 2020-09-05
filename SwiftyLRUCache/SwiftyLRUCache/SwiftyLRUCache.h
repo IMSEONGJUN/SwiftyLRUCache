a312481d41a8b552f6fdf1a3670d5e48001bfe83
SEONGJUN <naroti@naver.com>
2020-09-06 02:17:42 +0900

    Initial commit

---
+++ SwiftyLRUCache/SwiftyLRUCache/SwiftyLRUCache.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLRUCache.h
+//  SwiftyLRUCache
+//
+//  Created by SEONGJUN on 2020/09/06.
+//  Copyright © 2020 SEONGJUN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLRUCache.
+FOUNDATION_EXPORT double SwiftyLRUCacheVersionNumber;
+
+//! Project version string for SwiftyLRUCache.
+FOUNDATION_EXPORT const unsigned char SwiftyLRUCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLRUCache/PublicHeader.h>
+
+
